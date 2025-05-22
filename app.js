1abfbb4820281815e4ffb3981bfdb984911bc612
QuangNamTricker <105877568+QuangNamTricker@users.noreply.github.com>
2025-05-22 19:27:48 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+// app.js
+fetch("content.html")
+  .then(res => {
+    if (!res.ok) throw new Error("Không tìm thấy nội dung 😵");
+    return res.text();
+  })
+  .then(html => {
+    document.getElementById("root").innerHTML = html;
+  })
+  .catch(err => {
+    document.getElementById("root").innerHTML = `<p>Lỗi: ${err.message}</p>`;
+  });
